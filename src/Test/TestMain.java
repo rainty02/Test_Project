04401bf19c31adde089ff091f004820ddcbabe60
rainty02 <rainty02@gmail.com>
2021-06-25 12:03:58 +0900

    프로젝트 5차 수정

--- src/Test/TestMain.java
+++ src/Test/TestMain.java
@@ -1,18 +1,18 @@
-package Test;
-
-public class TestMain {
-
-	public static void main(String[] args) {
-		
-		SaleManager saleManager = new SaleManager(SaleDao.getInstance(), MenuDao.getInstance());
-		Login login = new Login(MemberDao.getInstance());
-		
-		saleManager.order("a");
-		saleManager.pay("a");
-
-		
-		
-		
-	}
-
-}
+//package Test;
+//
+//public class TestMain {
+//
+//	public static void main(String[] args) {
+//		
+//		SaleManager saleManager = new SaleManager(SaleDao.getInstance(), MenuDao.getInstance());
+//		Login login = new Login(MemberDao.getInstance());
+//		
+//		saleManager.order("a");
+//		saleManager.pay("a");
+//
+//		
+//		
+//		
+//	}
+//
+//}
